f2b5b1a94c8a23dd53cd16b4d7f6b5c060029acf
MarinsLucas <lucasmarinsramalho@gmail.com>
2023-08-31 09:45:40 -0300

    bateu

--- brickbrick.js
+++ brickbrick.js
@@ -6,6 +6,8 @@ import {
   setDefaultMaterial,
 } from "../libs/util/util.js";
 
+let gameStatus = 0; //0 = jogo não começou; 1 = jogo rolando ; 2 = jogo pausado 
+
 let scene, renderer, light, keyboard;
 scene = new THREE.Scene(); // Create main scene
 renderer = initRenderer(); // View function in util/utils
@@ -25,6 +27,7 @@ window.addEventListener(
 window.addEventListener("mousemove", onMouseMove);
 scene.add(camera);
 
+let collidableMeshList = [];
 let brickHolder = new THREE.Object3D();
 brickHolder.position.set(-1, 2.2, 0);
 scene.add(brickHolder);
@@ -37,10 +40,14 @@ let pad = createPad();
 let ball = createBall();
 
 let raycaster = new THREE.Raycaster();
-raycaster.layers.enable(0);
-camera.layers.enable(0);
+raycaster.layers.enable(1);
+camera.layers.enable(1);
+ball.layers.enable(1);
+//lista de objetos colidíveis. 
+
 
 let objects = [];
+//Essa hitbox, é o espaço onde o pad pode se movimentar!
 let hitbox = createHitbox();
 objects.push(hitbox);
 
@@ -56,17 +63,21 @@ function Brick(obj, resistance, isColliding) {
 }
 
 function onMouseMove(event) {
-  let pointer = new THREE.Vector2();
-  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-  pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  if(gameStatus == 0 || gameStatus == 1)
+  {
+    
+    let pointer = new THREE.Vector2();
+    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
-  raycaster.setFromCamera(pointer, camera);
-  // calculate objects intersecting the picking ray
-  let intersects = raycaster.intersectObjects(objects);
+    raycaster.setFromCamera(pointer, camera);
+    // calculate objects intersecting the picking ray
+    let intersects = raycaster.intersectObjects(objects);
 
-  if (intersects.length > 0) {
-    let point = intersects[0].point;
-    pad.position.set(point.x, -2.1, 0);
+    if (intersects.length > 0) {
+      let point = intersects[0].point;
+      pad.position.set(point.x, -2.1, 0);
+    }
   }
 }
 
@@ -86,11 +97,11 @@ function createHitbox() {
 
 function createBall() {
   var geometry = new THREE.SphereGeometry(0.05, 64, 32);
-  var material = new THREE.MeshBasicMaterial({ emissive: 0xffffff });
+  var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
   material.side = THREE.DoubleSide;
   var obj = new THREE.Mesh(geometry, material);
   obj.position.set(0, 0.1, 0);
-  pad.add(obj);
+  scene.add(obj);
   return obj;
 }
 
@@ -103,15 +114,6 @@ function createPad() {
   scene.add(obj);
   return obj;
 }
-
-/* function createCircle(){
-   var geometry = new THREE.CircleGeometry(1, 32);
-   var material = new THREE.MeshBasicMaterial({ emissive: 0xFFFFFF });
-   material.side = THREE.DoubleSide;
-   var obj = new THREE.Mesh(geometry, material);
-   obj.position.set(0, 0, 0);
-   scene.add(obj);
-} */
 
 function initializeMatrix(row) {
   // Initialize a 2D matrix with null values
@@ -202,36 +204,64 @@ function createBrick(x, y, resistance, brickHolder) {
   var obj = new THREE.Mesh(geometry, material);
   obj.position.set(x, y, 0);
   brickHolder.add(obj);
-
+  collidableMeshList.push(obj);
   const brick = new Brick(obj, resistance);
   return brick;
 }
 
+function updateBall()
+{
+  ball.translateX(0.01);
+  ball.translateY(0.01);
+
+  const raycaster = new THREE.Raycaster();
+  const direction = new THREE.Vector3(0.01, 0.01,0); 
+
+  raycaster.ray.origin.copy(ball.position);
+  raycaster.ray.direction.copy(direction);
+
+  const intersects = raycaster.intersectObjects(collidableMeshList);
+
+  if (intersects.length > 0 && intersects[0].distance <= 0.05) {
+    const collidedBrick = intersects[0].object;
+    console.log("Bateu");
+  }
+
+}
+
 function render() {
+  updateBall();
   requestAnimationFrame(render);
   renderer.render(scene, camera); // Render scene
 }
 
 function createBorders() {
+
   let upBorder = new THREE.BoxGeometry(3, 0.1, 0.1);
   let borderMaterial = setDefaultMaterial();
   borderMaterial.side = THREE.DoubleSide;
   let upb = new THREE.Mesh(upBorder, borderMaterial);
   upb.position.set(0.0, 2.5, 0.0);
   scene.add(upb);
+  collidableMeshList.push(upb);
 
   let leftBorder = new THREE.BoxGeometry(0.1, 10, 0.1);
   let lb = new THREE.Mesh(leftBorder, borderMaterial);
   lb.position.set(-1.25, 0.0, 0.0);
   scene.add(lb);
+  collidableMeshList.push(lb);
+
 
   let rightBorder = new THREE.BoxGeometry(0.1, 10, 0.1);
   let rb = new THREE.Mesh(rightBorder, borderMaterial);
   rb.position.set(1.25, 0.0, 0.0);
   scene.add(rb);
+  collidableMeshList.push(rb);
 
   let downBorder = new THREE.BoxGeometry(3, 0.1, 0.1);
   let db = new THREE.Mesh(downBorder, borderMaterial);
   db.position.set(0.0, -2.55, 0.0);
   scene.add(db);
+  collidableMeshList.push(db);
+
 }
