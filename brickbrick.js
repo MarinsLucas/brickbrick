d37e98375759db266563d91304e38086e35071ac
diasj21 <jonatas.dias@estudante.ufjf.br>
2023-08-31 17:42:52 -0300

    Plataforma funcionando sem raycaster e hitbox

--- brickbrick.js
+++ brickbrick.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { PointerLockControls } from "../build/jsm/controls/PointerLockControls.js";
 import KeyboardState from "../libs/util/KeyboardState.js";
 import {
   initDefaultSpotlight,
@@ -13,7 +14,6 @@ TODO: Fazer a plataforma parar quando encostar na parede
       Fazer a bola bater nos tijolos
 */
 
-
 let gameStatus = 0; //0 = jogo não começou; 1 = jogo rolando ; 2 = jogo pausado
 
 let scene, renderer, light, keyboard;
@@ -24,6 +24,7 @@ keyboard = new KeyboardState();
 
 // Main camera
 const camera = initializeCamera();
+const auxCamera = initializeCamera();
 scene.background = new THREE.Color(0x00008b);
 window.addEventListener(
   "resize",
@@ -32,8 +33,15 @@ window.addEventListener(
   },
   false
 );
-window.addEventListener("mousemove", onMouseMove);
+//window.addEventListener("mousemove", onMouseMove);
 scene.add(camera);
+
+const playerControls = new PointerLockControls(auxCamera, renderer.domElement);
+
+// Enable pointer lock when a user clicks on the canvas
+renderer.domElement.addEventListener("click", () => {
+  controls.lock();
+});
 
 let collidableMeshList = [];
 let brickHolder = new THREE.Object3D();
@@ -48,16 +56,24 @@ let pad = createPad();
 let padCollision = createPadCollision();
 let ball = createBall();
 
-let raycaster = new THREE.Raycaster();
-raycaster.layers.enable(1);
-camera.layers.enable(1);
-ball.layers.enable(1);
-//lista de objetos colidíveis.
+// Boolean flag to track whether the pointer is locked
+let isPointerLocked = false;
 
-let objects = [];
-//Essa hitbox é o espaço onde o pad pode se movimentar!
-let hitbox = createHitbox();
-objects.push(hitbox);
+// Listen for spacebar key press
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Space") {
+    togglePointerLock();
+  }
+});
+
+// Handle the pointer lock change event to update isPointerLocked flag
+playerControls.addEventListener("lock", () => {
+  isPointerLocked = true;
+});
+
+playerControls.addEventListener("unlock", () => {
+  isPointerLocked = false;
+});
 
 render();
 
@@ -70,7 +86,24 @@ function Brick(obj, resistance, isColliding) {
   this.isColliding = false;
 }
 
-function onMouseMove(event) {
+// Function to toggle pointer lock status
+function togglePointerLock() {
+  if (!isPointerLocked) {
+    playerControls.lock();
+    isPointerLocked = true;
+
+    // Add the mousemove event listener for locked pointer
+    document.addEventListener("mousemove", onMouseMoveLocked, false);
+  } else {
+    playerControls.unlock();
+    isPointerLocked = false;
+
+    // Remove the mousemove event listener when the pointer is unlocked
+    document.removeEventListener("mousemove", onMouseMoveLocked, false);
+  }
+}
+
+/* function onMouseMove(event) {
   const canvasBounds = renderer.domElement.getBoundingClientRect();
 
   if (gameStatus == 0 || gameStatus == 1) {
@@ -79,6 +112,16 @@ function onMouseMove(event) {
       ((event.clientX - canvasBounds.left) / canvasBounds.width) * 2 - 1;
     pointer.y =
       -((event.clientY - canvasBounds.top) / canvasBounds.height) * 2 + 1;
+
+    // Define the boundaries of the restricted area
+    const minX = -0.8; // Minimum x-coordinate within the area
+    const maxX = 0.8; // Maximum x-coordinate within the area
+    const minY = -0.8; // Minimum y-coordinate within the area
+    const maxY = 0.8; // Maximum y-coordinate within the area
+
+    // Clamp the pointer coordinates within the specified boundaries
+    pointer.x = Math.min(Math.max(pointer.x, minX), maxX);
+    pointer.y = Math.min(Math.max(pointer.y, minY), maxY);
 
     raycaster.setFromCamera(pointer, camera);
     // calculate objects intersecting the picking ray
@@ -89,20 +132,28 @@ function onMouseMove(event) {
       pad.position.set(point.x, -2.1, 0);
     }
   }
-}
+} */
 
-function createHitbox() {
-  var geometry = new THREE.BoxGeometry(2, 10, 1);
-  var material = new THREE.MeshStandardMaterial({
-    color: 0x00ff00, // Green color for the box
-    transparent: true, // Enable transparency
-    opacity: 0, // Set the opacity level (0: fully transparent, 1: fully opaque)
-  });
-  material.side = THREE.DoubleSide;
-  var obj = new THREE.Mesh(geometry, material);
-  obj.position.set(0, 0, 0);
-  scene.add(obj);
-  return obj;
+function onMouseMoveLocked(event) {
+  // Calculate the horizontal movement based on mouse position
+  const movementX =
+    event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+
+  // Define the horizontal movement speed
+  const horizontalSpeed = 0.01; // Adjust this value as needed
+
+  // Calculate the new x-coordinate for the platform
+  const newPlatformX = pad.position.x + movementX * horizontalSpeed;
+
+  // Define the boundaries of the restricted area
+  const minX = -0.9; // Minimum x-coordinate within the area
+  const maxX = 0.9; // Maximum x-coordinate within the area
+
+  // Clamp the new x-coordinate within the specified boundaries
+  const clampedX = Math.min(Math.max(newPlatformX, minX), maxX);
+
+  // Update the platform's position
+  pad.position.setX(clampedX);
 }
 
 function createBall() {
